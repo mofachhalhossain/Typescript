266485e23b6776ea5c1abe308a7a835af7265bbf
Ovi <mofachhalhossain101@gmail.com>
2024-02-27 23:36:29 +0600

    update

--- public/script.js
+++ public/script.js
@@ -4,7 +4,23 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const arr = [];
+const show = document.querySelector(".item-list");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, parseInt(amount.value));
+    const info = {
+        type: type.value,
+        from: tofrom.value,
+        details: details.value,
+        amount: parseInt(amount.value),
+    };
+    arr.push(info);
+    form.reset();
+    show === null || show === void 0 ? void 0 : show.innerHTML = '';
+    arr.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.textContent = JSON.stringify(item);
+        show === null || show === void 0 ? void 0 : show.appendChild(itemElement);
+    });
+    console.log(arr);
 });
--- src/script.ts
+++ src/script.ts
@@ -5,15 +5,30 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const arr: Array<object> = [];
+
+const show = document.querySelector(".item-list");
 
 form.addEventListener('submit', (e: Event)=>{
     e.preventDefault();
+    const info: object = {
+        type: type.value,
+        from: tofrom.value,
+        details: details.value,
+        amount: parseInt(amount.value),
+    }
+    
+    arr.push(info);
+    form.reset();
+    show?.innerHTML = '';
 
+    arr.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.textContent = JSON.stringify(item);
+        show?.appendChild(itemElement);
+    })
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        parseInt(amount.value),
-    )
+    console.log(arr);
+    
+
 })
